0e902953e6c0d0f5942dc8fb00aee6001f42d9db
fbelik <fjbelik@gmail.com>
2024-08-07 20:37:40 -0600

    allow fA to be Factorization, AbstractMatrix, Nothing, OR Number

--- src/models/linear_matrix_model.jl
+++ src/models/linear_matrix_model.jl
@@ -19,7 +19,7 @@ mutable struct LinearMatrixModel{NOUT} <: StationaryModel{NOUT}
     A_alloc::AbstractMatrix
     b_alloc::AbstractVector
     B_alloc::VectorOfVectors
-    fA::Union{Factorization,AbstractMatrix,Nothing}
+    fA::Union{Factorization,AbstractMatrix,Number,Nothing}
     p_last
 end
 
